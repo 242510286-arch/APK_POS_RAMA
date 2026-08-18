7184e6bf005f0b82f3c7d07298b239eba2c7bf38
242510286-arch <242510286@smkn4-tsm.sch.id>
2026-08-18 12:03:01 +0700

    Add files via upload

---
+++ app/Models/produk.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produk';
+
+    protected $fillable = [
+    'user_id',
+    'jenis_id',
+    'nama',
+    'harga_beli',
+    'harga_jual',
+    'stok',
+    'foto',
+];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
